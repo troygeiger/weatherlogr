de380ff155357671ce187c92a4b092e718b7699d
Troy Geiger <troygeiger@users.noreply.github.com>
2023-04-01 22:57:43 -0400

    Fixes update script

--- linux/update.sh
+++ linux/update.sh
@@ -1,2 +1,3 @@
-./install.sh --uninstall
-./install.sh
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+. $SCRIPT_DIR/install.sh --uninstall
+. $SCRIPT_DIR/install.sh
